211fd584649bc3e33398b4fc7638ebe6640698cd
rogervitor <rogervitorcarlessogoncalves@gmail.com>
2024-11-22 11:33:43 -0300

    Add files via upload

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -1,33 +1,20 @@
-const url = https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json
-async function visualizarInformacoesGlobais() {
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    console.log(dados);
-    }
-    async function visualizarInformacoesGlobais() {
-        const res = await fetch(url);
-        const dados = await res.json();
-        console.log(dados.tempo_medio);
-        }
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+    const horas = parseInt(dados.tempo_medio)
+    const minutos = Math.round((dados.tempo_medio - horas) * 100)
+    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo ) * 100).toFixed(2)
 
-        {
-            "total_pessoas_conectadas": 5.04e9,
-            "tempo_medio”: 2.38,
-            "total_pessoas_mundo": 7.888e9
-            }
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.`
 
-    visualizarInformacoesGlobais()
-    async function visualizarInformacoesGlobais() {
-        const res = await fetch(url)
-        const dados = await res.json()
-        const paragrafo = document.createElement("p")
-        paragrafo.classList.add("graficos-container__texto")
-       
-                        paragrafo.innerHTML = `Você sabia que o mundo tem
-                        <span>${dados.total_pessoas_mundo}</span> de pessoas
-                        e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede
-                        social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
+}
 
-        const container = document.getElementById("graficos-container")
-        container.appendChild(paragrafo)
-        }
+vizualizarInformacoesGlobais()
